74f2d66cbd8dcde33e3a6a09c0eff82714bba735
harsha <hreshavena@gmail.com>
2026-06-23 10:37:48 +0530

    sync

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Navbar = () => {
+  return (
+    <div>Navbar
+        <p> kjlngjfg </p>
+        
+    </div>
+   
+  )
+}
+
+export default Navbar
